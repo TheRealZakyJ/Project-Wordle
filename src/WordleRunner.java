7f4f8ddebea42057e5dc43814b43f349c7fbac7a
Zak <ZakhariasJoseph@gmail.com>
2022-09-06 09:41:21 -0500

    Main class(Wordle): Finished the checkWord method to compare the wordle word and guessed word.
    Runner Class(Wordle Runner): Made a for loop to run the scanner 5 times and checks if the guessed word matches the wordle word.

--- src/WordleRunner.java
+++ src/WordleRunner.java
@@ -6,12 +6,20 @@ public class WordleRunner {
         //System.out.println("wef1");
 
         Wordle wrd = new Wordle();
-       System.out.println(wrd);
-        System.out.println("dsflkn");
-       System.out.println(wrd);
-
+      // System.out.println(wrd);
+      //  System.out.println("dsflkn");
+       //System.out.println(wrd);
+        System.out.println(wrd.getWord());
 
         Scanner scan = new Scanner(System.in);
+
+        for(int i= 0; i<5;i++){
+            String wordGuess = scan.nextLine();
+
+            System.out.println(wrd.checkWord(wordGuess));
+
+        }
+
         //String wordInput = scan.nextLine();
         //System.out.println(wordInput);
        // System.out.println("wef2");
