997accd835471c14bc4c3c4421f5c52d62c68249
Zak <ZakhariasJoseph@gmail.com>
2022-09-12 09:32:00 -0500

    feat: Made a private variable to store each letter of the wordle word. fix: Changed private variables into final variables. fix: Gave private variables better names. fix: Cut down extra code from the checkWord method. chore: Complete the colorCheck method.

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,19 +1,16 @@
-import java.util.ArrayList;
-import java.io.File;
 import java.io.*;
 import java.util.*;
 
 
-
-
 public class Wordle {
-private String word;
-private ArrayList<String> wordList;
-
+private final String chosenWord;
+private final ArrayList<String> allWordList;
+private final ArrayList<String> chosenWordArray;
 
     public Wordle(){
 
-        wordList = new ArrayList<>();
+        allWordList = new ArrayList<>();
+        chosenWordArray = new ArrayList<>();
 
         try {
             File myObj = new File("Five Letter Words.txt");
@@ -23,7 +20,7 @@ private ArrayList<String> wordList;
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
               //  count++;
-                wordList.add(data);
+                allWordList.add(data);
                 //   System.out.println(data);
             }
            // System.out.println(count);
@@ -33,57 +30,102 @@ private ArrayList<String> wordList;
             e.printStackTrace();
         }
 
-        word = this.wordGen();
-        System.out.println(wordList.size());
+        chosenWord = this.wordGen();
+        System.out.println(allWordList.size());
+        for(int i = 0;i<5;i++){
+
+            chosenWordArray.add(chosenWord.substring(i,i+1));
+        }
+       // wordArray.add(word.substring(4,5));
         //System.out.println(myObj.length());
     }
 
+    public ArrayList<String> getWordArray(){
 
-    public String wordGen(){
-        //for(int i =0; i<=wordList.size();)
-        return wordList.get((int)(Math.random()*wordList.size()));
+        return chosenWordArray;
     }
 
-    public String getWord(){
+    public boolean checkArray(String gWord){
 
-        return word;
-    }
-
-    public boolean checkWord(String wordGuess){
-        if(wordGuess.length()!=5){
-            return false;
-        }
-        if(wordGuess.equals(word)){
-
-            return true;
-        }
-        if(wordGuess.substring(0,1).equals(word.substring(0,1))){
-
-            if(wordGuess.substring(1,2).equals(word.substring(1,2))){
-
-                if(wordGuess.substring(2,3).equals(word.substring(2,3))){
-
-                    if(wordGuess.substring(3,4).equals(word.substring(3,4))){
-
-                        if(wordGuess.substring(4,5).equals(word.substring(4,5))){
-
-                            return wordGuess.substring(5).equals(word.substring(5));
-                        }
-                    }
-                }
+        for (String s : allWordList) {
+            if (s.equals(gWord)) {
+                return true;
             }
         }
         return false;
     }
 
-    public String toString(){
-        return "Try to guess the word!";
+
+    public String wordGen(){
+        //for(int i =0; i<=wordList.size();)
+        return allWordList.get((int)(Math.random()*allWordList.size()));
     }
 
+    public String getWord(){
+
+        return chosenWord;
+    }
+
+    public boolean checkWord(String wordGuess){
+      /*  if(wordGuess.length()!=5){
+            return false;
+        }
+
+       */
+        return wordGuess.equals(chosenWord);
+        /*
+        if(wordGuess.toLowerCase(Locale.ROOT).substring(0,1).equals(word.substring(0,1))){
+
+            if(wordGuess.toLowerCase(Locale.ROOT).substring(1,2).equals(word.substring(1,2))){
+
+                if(wordGuess.toLowerCase(Locale.ROOT).substring(2,3).equals(word.substring(2,3))){
+
+                    if(wordGuess.toLowerCase(Locale.ROOT).substring(3,4).equals(word.substring(3,4))){
+
+                        return wordGuess.toLowerCase(Locale.ROOT).substring(4,5).equals(word.substring(4,5));
+
+                    }
+                }
+            }
+        }
+
+         */
+    }
+
+    public String colorCheck(String gWord){
+        //compare each letter:
+        //green if letter is in the right spot
+        // yellow if existing letter is in the wrong spot
+        //grey if letter does not exist in the word
+        if(gWord.length()!=5){
+            return "false";
+        }
+        ArrayList<String> gWordArray = new ArrayList<>();
+        for (int x =0;x<5;x++){
+            gWordArray.add(gWord.substring(x,x+1));
+        }
+        String greenReport = "";
+        String greyReport = "";
+        String yellowReport = "";
+      /*  for (int i = 0; i<chosenWordArray.size();i++){
+            for (int j = 0; i<gWordArray.size();j++){
+
+             //   if(){}
+            }
+        }
+
+       */
+        return "";
+    }
+
+    public String toString(){
+        return "";
+    }
+/*
     public static void main(String[] args) {
        //File words = new File("Five Letter Words.txt");
       // System.out.println(words.length());
-      /*  Scanner myReader = new Scanner(words);
+        Scanner myReader = new Scanner(words);
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
             System.out.println(data);
@@ -108,9 +150,9 @@ ArrayList<String> wordList = new ArrayList<>();
             FileReader frdr = new FileReader("Five Letter Words.txt");
             int z;
             while ((z = frdr.read()) != -1) {
-     */      //     wordList.add(z);
+           //     wordList.add(z);
               //  System.out.print((char) z);
 
         }
-        //    System.out.println(wordList.get(5));
+     */
     }
--- src/WordleRunner.java
+++ src/WordleRunner.java
@@ -6,18 +6,29 @@ public class WordleRunner {
         //System.out.println("wef1");
 
         Wordle wrd = new Wordle();
-      // System.out.println(wrd);
+       System.out.println(wrd);
+        //System.out.println(wrd.getWord());
+
       //  System.out.println("dsflkn");
        //System.out.println(wrd);
-        System.out.println(wrd.getWord());
+
+
+        System.out.println(wrd.getWordArray());
 
         Scanner scan = new Scanner(System.in);
 
         for(int i= 0; i<5;i++){
-            String wordGuess = scan.nextLine();
-
-            System.out.println(wrd.checkWord(wordGuess));
-
+            String wordGuess = scan.nextLine().toLowerCase();
+            System.out.println(wordGuess);
+            if(wordGuess.length()!=5 || !wrd.checkArray(wordGuess)){
+                i=i-1;
+                System.out.println("Please type a real 5 letter word.");
+            } else {
+              //  System.out.println(wrd.checkArray(wordGuess));
+                System.out.println(wrd.checkWord(wordGuess));
+                System.out.println("32");
+              //  System.out.println(wrd.colorCheck(wordGuess));
+            }
         }
 
         //String wordInput = scan.nextLine();
